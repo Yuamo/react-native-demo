458e03b7db59730d10e2bfc584f59cc8c48edb27
Han Sanye <5071@02.cn>
2017-07-10 18:39:13 +0800

    init

---
+++ App.js
@@ -0,0 +1,38 @@
+import React,{ Component, PropTypes }  from 'react';
+import {AppRegistry, Text, View, Button} from 'react-native';
+import {StackNavigator} from 'react-navigation';
+
+class ChatScreen extends  Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy'
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy{this.state}</Text>
+      </View>
+    );
+  }
+}
+
+class MainScreen extends Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (<Button
+      title="Go to Jane's profile"
+      onPress={() => navigate('Profile', {name: 'Jane'})}/>);
+  }
+}
+const App = StackNavigator({
+  Home: {
+    screen: MainScreen
+  },
+  Profile: {
+    screen: ChatScreen
+  }
+});
+
+export default App
